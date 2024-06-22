94c00d650ea5c6994f7a10d443f80ccd1dbfd403
RodionBulakh <bulakhrodion1@icloud.com>
2024-06-22 18:03:59 +0300

    change datepicker to use ID

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,7 +6,7 @@ const daysValue = document.querySelector('[data-days]');
 const hoursValue = document.querySelector('[data-hours]');
 const minutesValue = document.querySelector('[data-minutes]');
 const secondsValue = document.querySelector('[data-seconds]');
-const dateTimePicker = document.querySelector('#datetime-picker');
+const dateTimePicker = document.getElementById('datetime-picker');
 
 let countdownInterval = null;
 let selectedDate = null;
